61ddb4ab181730cec3644c3ecf97322e281d1d42
Ne0r0 <buflinskis@gmail.com>
2025-02-12 17:44:29 +0200

    ieskau e sakinyje

---
+++ ex6m.py
@@ -0,0 +1,28 @@
+# Parašykite programą, kuri pagal gautą tekstinę įvestį suskaičiuotų, kiek žodžių turi „e“ raidę.
+
+# Pavyzdys:
+
+# Įveskite tekstą: Šioje paskaitoje aptarsime papildomas Python integruotų duomenų struktūrų funkcijas.
+# Žodžių su 'e' skaičius: 5
+
+
+def word_with_e(text):
+    return len([word for word in user_input.split() if 'e' in word])
+
+user_input = input("Įveskite tekstą: ")
+words_count = word_with_e(user_input)
+print(f"Žodžių su 'e' skaičius: {words_count}")
+
+
+# user_input = input("Įveskite tekstą: ")
+# words_with_e = [word for word in user_input.split() if 'e' in word]
+# print(f"Žodžių su 'e' skaičius: {len(words_with_e)}")
+
+
+# user_input = input("Įveskite tekstą: ")
+
+# words = user_input.split()
+
+# count = sum(1 for word in words if 'e' in word)
+
+# print(f"Žodžių su 'e' skaičius: {count}")
