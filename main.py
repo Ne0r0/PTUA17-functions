07b3bf6ecd8312f48cf4758921e6d256a8ba022a
Ne0r0 <buflinskis@gmail.com>
2025-02-18 21:07:22 +0200

    something

--- main.py
+++ main.py
@@ -27,22 +27,43 @@
 
 
 
-def multiply(num_1: int, num_2: int) -> int:
-    return num_1 * num_2
+# def multiply(num_1: int, num_2: int) -> int:
+#     return num_1 * num_2
 
 
-print(multiply(2, 2))
+# print(multiply(2, 2))
 
 
-multiplication = lambda num_1, num_2: num_1 * num_2
+# multiplication = lambda num_1, num_2: num_1 * num_2
 
-print(multiplication(2, 7))
+# print(multiplication(2, 7))
 
-answer = (lambda num_1, num_2: num_1 * num_2)(2, 7)
-print(answer)
+# answer = (lambda num_1, num_2: num_1 * num_2)(2, 7)
+# print(answer)
 
-a = 5
-b = 7
+# a = 5
+# b = 7
 
-answer = (lambda num_1, num_2: num_1 * num_2)(a, b)
-print(answer)
+# answer = (lambda num_1, num_2: num_1 * num_2)(a, b)
+# print(answer)
+
+
+try:
+    print("I am here!")
+    print(2 / 0)
+except Exception as e:
+    print(f"Learn some basic math dude!,because off error: {e}")
+
+
+try:
+    # print("I am here!")
+    print(2 / 0)
+except ZeroDivisionError:
+    print(f"CAN'T DIVIDE BY ZERO!")
+
+
+try:
+    input = int(input("Enter a number: "))
+    print(input)
+except Exception as e:
+    print(f"Error: {e}")
